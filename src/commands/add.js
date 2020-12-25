0b9051a67a1abd399ee765bcf4be17104f6df2eb
JMax45 <maxmailru.45@gmail.com>
2020-12-25 19:54:03 +0100

    fix: validation in add command
    Added some basic validation in add command which adds/removes the -100
    in chat id.
    Also moved the schema.sql file.

--- src/commands/add.js
+++ src/commands/add.js
@@ -5,12 +5,17 @@ module.exports = {
     description: 'Create a new redirect',
     public: true, 
     execute(ctx){
+        const { splitArgs } = ctx.state.command;
+        splitArgs[1] = splitArgs[1].startsWith('-100') ? splitArgs[1].substring(4) : splitArgs[1];
+        splitArgs[2] = splitArgs[2].startsWith('-100') ? splitArgs[2] : `-100${splitArgs[2]}`;
         db.query({
             text: 'INSERT INTO redirect(title, origin, destination) VALUES($1, $2, $3)',
-            values: ctx.state.command.splitArgs
+            values: splitArgs
         }, (err, res) => {
             if(err){
                 ctx.reply(err.message)
+            } else {
+                ctx.reply('Redirect added successfully');
             }
         })
     }
