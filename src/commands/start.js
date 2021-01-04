d718eacda5b95513bb86eae391a0c321051f5cc0
JMax45 <maxmailru.45@gmail.com>
2021-01-04 11:52:24 +0100

    feat: start command message

--- src/commands/start.js
+++ src/commands/start.js
@@ -3,6 +3,9 @@ module.exports = {
     description: 'Receive a greeting from the bot',
     public: true, 
     execute(ctx){
-        ctx.replyWithMarkdown('Hello World');
+        const message = '👋 Hi, I can redirect messages from various chats, channels and groups to your own ones.'
+            + '\nIf you\'re using this bot for the first time please authenticate with /auth.'
+            + ' You can create new redirects using /add and manage them with /dashboard.'
+        ctx.replyWithMarkdown(message);
     }
 }
